447cde86b9ce982b317ba0949a6e1a6dd853b250
ed-sa-ma <28660051+ed-sa-ma@users.noreply.github.com>
2021-06-27 17:50:37 +1100

    Adding adapter and Netlify config for hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,14 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		// We disable router to let the browser handle our hash navigations, since they won't produce reloads.
